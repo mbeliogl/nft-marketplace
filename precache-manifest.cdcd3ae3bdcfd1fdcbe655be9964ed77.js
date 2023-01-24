d65d58e456fd4898436e0a9d55d7d11f9e5154da
mbeliogl <maximbelioglo@gmail.com>
2023-01-23 21:29:23 -0500

    Updates

---
+++ precache-manifest.cdcd3ae3bdcfd1fdcbe655be9964ed77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4b2aca1beeb77aa06c5",
+    "url": "/nft-marketplace/static/css/main.a80e6fdb.chunk.css"
+  },
+  {
+    "revision": "a4b2aca1beeb77aa06c5",
+    "url": "/nft-marketplace/static/js/main.a4b2aca1.chunk.js"
+  },
+  {
+    "revision": "ec21fda1ea85faebefa6",
+    "url": "/nft-marketplace/static/css/1.a4bd2af7.chunk.css"
+  },
+  {
+    "revision": "ec21fda1ea85faebefa6",
+    "url": "/nft-marketplace/static/js/1.ec21fda1.chunk.js"
+  },
+  {
+    "revision": "559f3e4211ebc7bb7f40",
+    "url": "/nft-marketplace/static/js/runtime~main.559f3e42.js"
+  },
+  {
+    "revision": "7b80ce6f2a0c461feab06b71394c4400",
+    "url": "/nft-marketplace/index.html"
+  }
+];
